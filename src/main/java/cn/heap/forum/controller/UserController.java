7d8282d481f3760e4c877f7d8015947e57f9097f
abc <387881061@qq.com>
2025-07-07 21:10:07 +0800

    7-7user2

--- src/main/java/cn/heap/forum/controller/UserController.java
+++ src/main/java/cn/heap/forum/controller/UserController.java
@@ -1,7 +1,10 @@
 package cn.heap.forum.controller;
 
+import io.swagger.annotations.Tag;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "用户接口")
 public class UserController {
+    
 }
