c4b905cd732e54bb33b4c22a53b6327c83b785b4
abc <387881061@qq.com>
2025-07-07 21:16:38 +0800

    7-7user2

--- src/main/java/cn/heap/forum/controller/UserController.java
+++ src/main/java/cn/heap/forum/controller/UserController.java
@@ -1,10 +1,37 @@
 package cn.heap.forum.controller;
 
+import cn.heap.forum.pojo.User;
+import cn.heap.forum.service.UserService;
+import cn.heap.forum.util.ServerResult;
+import com.baomidou.mybatisplus.extension.api.R;
 import io.swagger.annotations.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Tag(name = "用户接口")
 public class UserController {
-    
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/user/login") // 登录接口,身份判断
+    public ServerResult<List<User>> login(){
+        return null;
+    }
+
+    @RequestMapping("/user/register") // 注册接口
+    public ServerResult<List<User>> register(){
+        return null;
+    }
+
+    @RequestMapping("/user/update")
+    public ServerResult<List<User>> update(){
+        return null;
+    }
+
+
+
 }
